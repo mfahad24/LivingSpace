bd707c2febc16e42b890c280356af3ed4c1e5dd5
Mohammed Fahad <mfahad24@users.noreply.github.com>
2018-05-28 12:09:06 -0400

    all done but mobile

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// (function (){
 
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -15,16 +14,20 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
 var productsdiv = document.getElementById("productsdiv");
@@ -40,58 +43,58 @@ var xout = document.getElementById("xout")
 
 productsdiv.style.display = "none";
 
-product.onclick = function () {
-  productsdiv.style.display  = "block";
+product.onclick = function() {
+  productsdiv.style.display = "block";
 }
 
-close.onclick = function () {
+close.onclick = function() {
   productsdiv.style.display = "none";
 }
 
-textone.onmouseover = function () {
+textone.onmouseover = function() {
   textone.style.width = "60%";
 }
 
-textone.onmouseout = function () {
+textone.onmouseout = function() {
   textone.style.width = "50%";
 }
 
 
 
-texttwo.onmouseover = function () {
+texttwo.onmouseover = function() {
   texttwo.style.width = "60%";
 }
 
-texttwo.onmouseout = function () {
+texttwo.onmouseout = function() {
   texttwo.style.width = "50%";
 }
 
 
 
-textthree.onmouseover = function () {
+textthree.onmouseover = function() {
   textthree.style.width = "70%";
 }
 
-textthree.onmouseout = function () {
+textthree.onmouseout = function() {
   textthree.style.width = "60%";
 }
 
-textfour.onmouseover = function () {
+textfour.onmouseover = function() {
   textfour.style.width = "70%";
 }
 
-textfour.onmouseout = function () {
+textfour.onmouseout = function() {
   textfour.style.width = "60%";
 }
 
 
 mobilenav.style.display = "none"
 
-viewheadline.onclick = function () {
+viewheadline.onclick = function() {
   mobilenav.style.display = "block";
 }
 
-xout.onclick = function () {
+xout.onclick = function() {
   mobilenav.style.display = "none";
 }
 
